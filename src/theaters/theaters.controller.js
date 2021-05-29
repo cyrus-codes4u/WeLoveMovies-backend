a41645c3858a8856f08ead3e1ddffacd60262f62
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-05-29 15:53:04 -0400

    Implemented GET /theaters rquest handling to list all theaters with their movies

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,5 +1,22 @@
+const service = require("./theaters.service")
+const reduceProperties = require("../utils/reduce-properties");
 
+async function list(req,res,next){
+    const theatersAndMovies = await service.list()
+    const reduceTheaterAndMovies = reduceProperties("theater_id", {
+        movie_id: ["movies", null, "movie_id"],
+        title: ["movies", null, "title"],
+        rating: ["movies", null, "rating"],
+        is_showing: ["movies", null, "is_showing"],
+        image_url: ["movies", null, "image_url"],
+        description: ["movies", null, "description"],
+        runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+      });
+
+
+    res.status(200).send({data: reduceTheaterAndMovies(theatersAndMovies)})
+}
 
 module.exports ={
-    list: () => null,
+    list: list,
 }
---
+++ src/theaters/theaters.service.js
@@ -0,0 +1,12 @@
+const knex = require("../db/connection")
+
+function list(){
+    return knex("movies_theaters as mt")
+        .join("theaters as t", "mt.theater_id", "t.theater_id")
+        .join("movies as m", "mt.movie_id", "m.movie_id")
+        .select("*")
+}
+
+module.exports = {
+    list
+}
